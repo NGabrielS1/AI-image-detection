4f3ebb8aa8d0095e17311e8bb93a7d1f29404035
NGabrielS1 <gabrielsulaiman08@gmail.com>
2025-01-30 22:59:59 +0700

    code changes

--- neural_network/train.py
+++ neural_network/train.py
@@ -24,18 +24,16 @@ class CreateDataset(Dataset):
     def __getitem__(self,index):
         img0_tuple = self.imageFolderDataset.imgs[index]
 
-        #We need to approximately 50% of images to be in the same class
-        should_get_same_class = random.randint(0,1) 
-        if should_get_same_class:
+        # Determine if class is same or different
+        class_type = random.randint(0,1)
+
+        if class_type: # Find Image with same class 
             while True:
-                #Look untill the same class image is found
                 img1_tuple = random.choice(self.imageFolderDataset.imgs) 
                 if img0_tuple[1] == img1_tuple[1]:
                     break
-        else:
-
+        else: # Find Image with different class
             while True:
-                #Look untill a different class image is found
                 img1_tuple = random.choice(self.imageFolderDataset.imgs) 
                 if img0_tuple[1] != img1_tuple[1]:
                     break

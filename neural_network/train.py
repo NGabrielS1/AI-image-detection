0b70f3902a395aaa51735b8228d7b53780ebedaa
NGabrielS1 <gabrielsulaiman08@gmail.com>
2025-01-30 20:49:28 +0700

    edited code

--- neural_network/train.py
+++ neural_network/train.py
@@ -17,5 +17,11 @@ class SiameseNeuralNetwork(nn.module):
         super().__init__()
         # load ResNet50 (transfer learning)
         self.resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
-        # change output and input
-        self.resnet50.conv2d
+        # change input and output
+        self.resnet50.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.resnet50.fc = nn.Linear(in_features=2048, out_features=2, bias=True)
+    def forward(self, X1, X2):
+        y1 = self.resnet50(X1)
+        y2 = self.resnet50(X2)
+
+        return y1, y2

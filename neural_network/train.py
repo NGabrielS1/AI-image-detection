37c4cfd2afacaffa580e1e678bdfc7fa72619c20
NGabrielS1 <gabrielsulaiman08@gmail.com>
2025-01-31 09:04:01 +0700

    code changes

--- neural_network/train.py
+++ neural_network/train.py
@@ -92,4 +92,4 @@ class ContrastiveLoss(torch.nn.Module):
 # create a instance of model, choose loss function and optimizer
 model = SiameseNetwork()
 criterion = ContrastiveLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.0005 )
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.0005)

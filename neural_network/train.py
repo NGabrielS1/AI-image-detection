2a23e6e3a51b5909d5467f83dcd2a197ff70eea9
NGabrielS1 <gabrielsulaiman08@gmail.com>
2025-02-04 19:27:21 +0700

    changes

--- neural_network/train.py
+++ neural_network/train.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
     # create a instance of model, choose loss function and optimizer
     model = SiameseNetwork().to(device)
     criterion = ContrastiveLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr = 0.005, weight_decay=0.001)
+    optimizer = torch.optim.AdamW(model.parameters(), lr = 0.01, weight_decay=0.001)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[3,7], gamma=0.1)
 
     # variables

8695629f69a4a820cf1d46bc7ca5b26fa7bde8b7
NGabrielS1 <gabrielsulaiman08@gmail.com>
2025-02-09 20:14:07 +0700

    app changes

--- neural_network/app.py
+++ neural_network/app.py
@@ -31,11 +31,14 @@ class App(ctk.CTk):
         self.upload_img = ctk.CTkImage(Image.open(current_path+"/upload-button.png"), size=(300, 75))
         self.bg_image_pil = Image.open(current_path+"/background.png")
         self.bg_img = ctk.CTkImage(self.bg_image_pil,size=(self.width, self.height))
-        self.page_img = ctk.CTkImage(Image.open(current_path+"/page.png"), size=(self.width, self.height))
+        self.page_img_pil = Image.open(current_path+"/page.png")
+        self.page_img = ctk.CTkImage(self.page_img_pil, size=(self.width, self.height))
+        self.placeholder_image = ctk.CTkImage(self.page_img_pil.crop((260, 135, 260+287, 135+287)).convert("RGBA"), size=(287,287))
+
 
         # fonts
-        LS_font = current_path+"/LeagueSpartan-Bold.ttf"
-        M_font = current_path+"/Montserrat-Medium.ttf"
+        self.LS_font = current_path+"/LeagueSpartan-Bold.ttf"
+        self.M_font = current_path+"/Montserrat-Medium.ttf"
 
         # background image
         self.bg_image_label = ctk.CTkLabel(master=self, image=self.bg_img, text="")
@@ -43,15 +46,15 @@ class App(ctk.CTk):
 
         # title page
         self.welcome_label = ctk.CTkLabel(master=self)
-        self.welcome_label.configure(image=self.transparent(55, 90, widget=self.welcome_label, text_func=True, text="WELCOME TO", font=M_font))
+        self.welcome_label.configure(image=self.transparent(55, 90, widget=self.welcome_label, text_func=True, text="WELCOME TO", font=self.M_font))
         self.welcome_label.place(x=55,y=90)
 
         self.title_label = ctk.CTkLabel(master=self)
-        self.title_label.configure(image=self.transparent(55, 122, widget=self.title_label, text_func=True, text="AI IMAGE \nDETECTOR", font=LS_font, font_size=98))
+        self.title_label.configure(image=self.transparent(55, 122, widget=self.title_label, text_func=True, text="AI IMAGE \nDETECTOR", font=self.LS_font, font_size=98))
         self.title_label.place(x=55,y=122)
 
-        self.start_btn = ctk.CTkButton(master=self, width=200, height=50, text="", corner_radius=50, fg_color="#0fb568", hover=False, bg_color="black", text_color="black", font=("Free Sans", 20), \
-            image=self.transparent(55, 450, text_func=True, text="START NOW", font=M_font, font_size=17, bg_color=(15, 181, 104),pad=(17,0), color=(0,0,0)))
+        self.start_btn = ctk.CTkButton(master=self, width=200, height=50, text="", corner_radius=50, fg_color="#0fb568", hover=False, bg_color="black", text_color="black", font=("Free Sans", 20), 
+            image=self.transparent(55, 450, text_func=True, text="START NOW", font=self.M_font, font_size=17, bg_color=(15, 181, 104),pad=(17,0), color=(0,0,0)))
         self.start_btn.place(x=55,y=450)
 
         self.welcome_label.destroy()
@@ -66,12 +69,15 @@ class App(ctk.CTk):
         self.footer.columnconfigure((0,1), weight=1, uniform=True)
         self.footer.rowconfigure(0, weight=1)
 
-        self.upload_btn = ctk.CTkButton(self.footer, height=75, width=350, image=self.upload_img, text="", fg_color="#171717", corner_radius=0, anchor="center")
+        self.upload_btn = ctk.CTkButton(self.footer, height=75, width=350, image=self.upload_img, text="", fg_color="#171717", corner_radius=0, anchor="center", command=self.get_images)
         self.upload_btn.pack(side="left", fill="both")
 
-        self.file_name_label = ctk.CTkLabel(self.footer, height=75, text="", fg_color="transparent", corner_radius=0,\
-            image=self.transparent(0,0, text_func=True, text="FILE NAME", font=M_font, font_size=30, bg_color=(23, 23, 23), pad=(0,0)))
+        self.file_name_label = ctk.CTkLabel(self.footer, height=75, text="", fg_color="transparent", corner_radius=0)
+        self.file_name_label.configure(image=self.transparent(0,0, widget=self.file_name_label, text_func=True, text="FILE NAME", font=self.M_font, font_size=30, bg_color=(23, 23, 23)))
         self.file_name_label.pack(side="right", fill="both")
+
+        self.image_label = ctk.CTkLabel(self, height=287, width=287, text="", image=self.placeholder_image)
+        self.image_label.place(x=260,y=135)
 
     # helper functions
     def transparent(self, x, y, widget=None, text_func=False, text=None, font=None, color=None, font_size=None, width=None, height=None, bg_color=None, pad=None, anchor=None):
@@ -90,6 +96,10 @@ class App(ctk.CTk):
         # draw text
         if text_func:
             width = len(text)*font_size
+            try:
+                if widget == self.file_name_label and width > 400:
+                    width = 400
+            except: pass
             height = font_size+5
             fill = color
             text = text
@@ -113,13 +123,15 @@ class App(ctk.CTk):
         image = ctk.CTkImage(image, size=(width,height))
         return image
 
-    # def get_images(self):
-    #     self.file = 0
-    #     self.files = filedialog.askopenfiles(filetypes=[("Image Files", "*.jpg *.jpeg *.pgm")])
-    #     print(self.files[0])
-    #     self.file_name.configure(text=f"{self.files[self.file].name}")
-    #     self.file_counter_label.configure(text=f"Files: {len(self.files)}")
-    #     threading.Thread(target=self.process_image, args=(self.files[self.file].name,)).start()
+    def get_images(self):
+        self.file = 0
+        self.files = filedialog.askopenfiles(filetypes=[("Image Files", "*.jpg *.jpeg *.pgm")])
+        print(self.files[0])
+        filename = self.files[self.file].name.split("/")[-1]
+        self.file_name_label.configure(image=self.transparent(0,0, widget=self.file_name_label, text_func=True, text=f"{filename}", \
+            font=self.M_font, font_size=30, bg_color=(23, 23, 23)))
+        # self.file_counter_label.configure(text=f"Files: {len(self.files)}")
+        threading.Thread(target=self.process_image, args=(self.files[self.file].name,)).start()
 
     # def next_file(self):
     #     if self.file < len(self.files)-1:
@@ -135,10 +147,10 @@ class App(ctk.CTk):
     #         self.file_counter_label.configure(text=f"Files: {len(self.files)}")
     #         self.image_label.configure(image=None)
 
-    # def process_image(self, file):
-    #     image = Image.open(file)
-    #     image.resize((256,256))
-    #     self.image_label.configure(image=ctk.CTkImage(image,size=(self.image_label.winfo_width(),self.image_label.winfo_width())))
+    def process_image(self, file):
+        image = Image.open(file)
+        image.resize((287,287))
+        self.image_label.configure(image=ctk.CTkImage(image,size=(self.image_label.winfo_width(),self.image_label.winfo_width())))
 
 # Run application
 if __name__ == "__main__":
